bc9ff944cea6e075d300895a5b1f1bb04141a8ae
KyleChiasson <kc428921@ohio.edu>
2026-03-20 16:45:39 -0400

    Updated Navbar Links

--- functionality.js
+++ functionality.js
@@ -3,10 +3,10 @@ function init(){
     if(navbar != undefined)
         navbar.innerHTML = /*html*/`
             <ul>
-                <li><a href='index.html'>Home</a></li>
-                <li><a href='experiences.html'>Experiences</a></li>
-                <li><a href='pathway-reflection.html'>Pathway Reflection</a></li>
-                <li><a href='ohp-reflection.html'>OHP Reflection</a></li>
+                <li><a href='/'>Home</a></li>
+                <li><a href='/experiences'>Experiences</a></li>
+                <li><a href='/pathway-reflection'>Pathway Reflection</a></li>
+                <li><a href='/ohp-reflection'>OHP Reflection</a></li>
             </ul>
         `
 }
