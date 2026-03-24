a34a0783f8785a8c8be06337b050ccd89cf77059
Kyle Chiasson <kc428921@ohio.edu>
2026-03-23 20:51:27 -0400

    Updated Navbar Styles

--- functionality.js
+++ functionality.js
@@ -3,10 +3,10 @@ function init(){
     if(navbar != undefined)
         navbar.innerHTML = /*html*/`
             <ul>
-                <li><a href='/honors-portfolio/'>Home</a></li>
-                <li><a href='/honors-portfolio/experiences'>Experiences</a></li>
-                <li><a href='/honors-portfolio/pathway-reflection'>Pathway Reflection</a></li>
-                <li><a href='/honors-portfolio/ohp-reflection'>OHP Reflection</a></li>
+                <li class='home'             ><a href='/honors-portfolio/'>Home</a></li>
+                <li class='experiences'       ><a href='/honors-portfolio/experiences'>Experiences</a></li>
+                <li class='pathway-reflection'><a href='/honors-portfolio/pathway-reflection'>Pathway Reflection</a></li>
+                <li class='ohp-reflection'    ><a href='/honors-portfolio/ohp-reflection'>OHP Reflection</a></li>
             </ul>
         `
 }
