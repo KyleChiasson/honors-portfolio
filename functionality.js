50149ae9519be4c9dfdc32ab771a972aba43bb6c
KyleChiasson <kc428921@ohio.edu>
2026-03-25 16:57:13 -0400

    Added Styles

--- functionality.js
+++ functionality.js
@@ -3,10 +3,16 @@ function init(){
     if(navbar != undefined)
         navbar.innerHTML = /*html*/`
             <ul>
-                <li class='home'             ><a href='/honors-portfolio/'>Home</a></li>
-                <li class='experiences'       ><a href='/honors-portfolio/experiences'>Experiences</a></li>
-                <li class='pathway-reflection'><a href='/honors-portfolio/pathway-reflection'>Pathway Reflection</a></li>
-                <li class='ohp-reflection'    ><a href='/honors-portfolio/ohp-reflection'>OHP Reflection</a></li>
+                <li class='name' style='padding-right: 5%'><h3>Kyle Chiasson</h3></li>
+                <li class='home'               ><a href='/honors-portfolio/'>Home</a></li>
+                <li class='experiences'        ><a href='/honors-portfolio/experiences'>Experiences</a></li>
+                <li class='pathway-reflection' ><a href='/honors-portfolio/pathway-reflection'>Pathway Reflection</a></li>
+                <li class='ohp-reflection'     ><a href='/honors-portfolio/ohp-reflection'>OHP Reflection</a></li>
             </ul>
+        `
+    if(footer != undefined)
+        footer.innerHTML =  /*html*/`
+            <p>Images on this website belong to their respective organizations.</p>
+            <p>All written work on this website was made and owned by Kyle Chiasson.</p>
         `
 }
